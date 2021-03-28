63d50e5796a133009f7953dc337fa385d3e06c26
Yurella86 <78345730+Yurella86@users.noreply.github.com>
2021-03-28 16:32:04 +0300

    add message

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 
-let text = document.getElementById('input-text');
-let button = document.querySelector('.add');
-button.addEventListener('click', function () {
-    alert('123')
-})
+let inputText = document.getElementById('input-text');
+let buttonAdd = document.querySelector('.add');
+let buttonClose = document.getElementById('del0')
+let containerCards = document.querySelector('.card-wrapper')
+
+let listMessage = [];
+
+buttonAdd.addEventListener('click', function () {
+    let newTodo = {
+        todo: inputText.value,
+        checked: false,
+        important: false
+    };
+    listMessage.push(newTodo);
+    newMessage()
+});
+
+buttonClose.addEventListener('click', function () {
+    console.log('123');
+});
+
+function newMessage() {
+    let displayMessage = '';
+    listMessage.forEach(function (elem, index) {
+        displayMessage += `
+        <li class="message">
+                <div>
+                    <h3>${elem.todo}</h3>
+                </div>
+                <div>
+                <button id="del${index}" class="close"><img src="./img/close.png" alt=""></button>
+                </div>
+            </li>
+        `;
+        containerCards.innerHTML = displayMessage;
+    })
+}
