20fef7a9a1407ee4ab9f4444b1f70b4753987a66
Yurella86 <78345730+Yurella86@users.noreply.github.com>
2021-03-30 19:38:06 +0300

    download from server 19:37

--- script.js
+++ script.js
@@ -1,46 +1,71 @@
-
-let inputText = document.getElementById('input-text');
-let buttonAdd = document.querySelector('.add');
-let buttonClose = document.querySelector('.close')
-let cardWrapper = document.querySelector('.card-wrapper');
+let newText = document.getElementById('text');
+let add = document.getElementById('add');
+let cards = document.getElementById('cards');
 
 
-let listMessage = [];
-let listDelId = [];
-let s = 0;
-
-buttonAdd.addEventListener('click', function () {
-    let newTodo = { todo: inputText.value };
-
-    if (inputText.value == "") {
-        alert('Please enter a new message!');
-    } else {
-        listMessage.push(newTodo);
-        newMessage()
-        listDelId.push("del" + s)
-        console.log(listDelId)
-        s += 1;
-    }
-});
-
-buttonClose.addEventListener('click', function () {
-    alert('1')
+add.addEventListener('click', function () {
+  if (newText.value === '') {
+    alert('Please enter a new message!')
+  } else {
+    //sendObject(newText.value);
+    loadNewName()
+  }
 })
 
-function newMessage() {
-    let displayMessage = '';
-    listMessage.forEach(function (elem, index) {
-        displayMessage += `
-            <li class="message">
-                <h3>${elem.todo}</h3>
-                <button id="del${index}" class="close">X</button>
-            </li>
-        `;
-        cardWrapper.innerHTML = displayMessage;
-        inputText.value = '';
-    })
+
+async function sendObject(text) {
+  let user = {
+    name: text
+  };
+
+  let res = await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(user)
+  });
+  let result = await res.json();
+  alert(result.message);
+
 }
 
+async function loadNewName() {
+  let res = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+  let arrayObject = await res.json()
+  for (const key in arrayObject) {
+    const elementObject = arrayObject[key];
+    let li = document.createElement('li')
+    li.className = 'message';
+    cards.appendChild(li)
 
+    let h3 = document.createElement('h3')
+    h3.innerHTML = elementObject;
+    li.appendChild(h3);
+    let buttonClose = document.createElement('button')
+    buttonClose.id = 'del';
+    buttonClose.className = 'close';
+    buttonClose.innerHTML = 'X';
+    li.appendChild(buttonClose);
+  }
 
+}
 
+function createNewEl(text) {
+  let li = document.createElement('li')
+  let h3 = document.createElement('h3')
+  let buttonClose = document.createElement('button')
+
+  h3.innerHTML = text;
+  li.className = 'message';
+  li.appendChild(h3);
+  li.appendChild(buttonClose);
+  buttonClose.id = 'del';
+  buttonClose.className = 'close';
+  buttonClose.innerHTML = 'X';
+  cards.appendChild(li)
+
+  buttonClose.addEventListener('click', function () {
+    cards.removeChild(li)
+  })
+}
