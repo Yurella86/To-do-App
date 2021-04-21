f52d9295863c7f0f512c3850efac71375d193762
YurellaV <yurturv@gmail.com>
2021-04-21 16:37:21 +0300

    new_file_2.txt

--- script.js
+++ script.js
@@ -1,93 +1,14 @@
 let newText = document.getElementById('text');
-let add = document.getElementById('add');
-let cards = document.getElementById('cards');
-
-document.addEventListener('DOMContentLoaded', loadPage)
-
-async function loadPage() {
-  let res = await fetch('https://jsonplaceholder.typicode.com/posts')
-  let arrayObject = await res.json();
-  arrayObject = arrayObject.splice(0, 2);
-  for (const key in arrayObject) {
-    const elementObject = arrayObject[key];
-    let li = document.createElement('li')
-    li.className = 'message';
-    cards.appendChild(li)
-
-    let h3 = document.createElement('h3')
-    h3.innerHTML = elementObject.title;
-    li.appendChild(h3);
-
-    let buttonClose = document.createElement('button')
-    buttonClose.id = 'del';
-    buttonClose.className = 'close';
-    buttonClose.innerHTML = 'X';
-    li.appendChild(buttonClose);
-  };
-};
+let button = document.getElementById('add');
 
 
-add.addEventListener('click', function () {
-  if (newText.value === '') {
-    alert('Please enter a new message!')
-  } else {
-    //sendObject(newText.value);
-    loadNewName()
-  }
-})
+button.addEventListener("click", async function () {
+  await newMessage();
+});
 
-
-async function sendObject(text) {
-  let user = {
-    title: text
-  };
-  let res = await fetch('https://github.com/Yurella86/To-do-App/blob/main/db.json', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(user)
-  });
-  let result = await res.json();
-  alert(result)
+async function newMessage() {
+  let res = await fetch("https://606efbdf0c054f001765814c.mockapi.io/api/v1/messages");
+  let body = res.json();
+  return console.log('1');
 }
 
-async function loadNewName() {
-  let res = await fetch('https://jsonplaceholder.typicode.com/posts')
-  let arrayObject = await res.json();
-  for (const key in arrayObject) {
-    const elementObject = arrayObject[key];
-    let li = document.createElement('li')
-    li.className = 'message';
-    cards.appendChild(li)
-
-    let h3 = document.createElement('h3')
-    h3.innerHTML = elementObject.title;
-    li.appendChild(h3);
-
-    let buttonClose = document.createElement('button')
-    buttonClose.id = 'del';
-    buttonClose.className = 'close';
-    buttonClose.innerHTML = 'X';
-    li.appendChild(buttonClose);
-  };
-};
-/*
-function createNewEl(text) {
-  let li = document.createElement('li')
-  let h3 = document.createElement('h3')
-  let buttonClose = document.createElement('button')
-
-  h3.innerHTML = text;
-  li.className = 'message';
-  li.appendChild(h3);
-  li.appendChild(buttonClose);
-  buttonClose.id = 'del';
-  buttonClose.className = 'close';
-  buttonClose.innerHTML = 'X';
-  cards.appendChild(li)
-
-  buttonClose.addEventListener('click', function () {
-    cards.removeChild(li)
-  })
-}*/
