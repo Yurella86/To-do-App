8a9aab9b97a36777f321399d3a17b363a7c7aec9
Turko Yuriy <yurella1986@gmail.com>
2021-07-15 21:24:50 +0300

    only work dblClick

--- script.js
+++ script.js
@@ -1,14 +1,81 @@
 let newText = document.getElementById('text');
-let button = document.getElementById('add');
+let addTodo = document.getElementById('add');
+let cards = document.getElementById('cards');
 
+let lengthItems = 0;
+const URL = "https://606efbdf0c054f001765814c.mockapi.io/api/v1/messages"
+const newObj = {
+  id: null,
+  title: newText.value
+};
 
-button.addEventListener("click", async function () {
-  await newMessage();
+addTodo.addEventListener("click", (el) => {
+  el.preventDefault()
+
+  if (newText.value) {
+    newObj.title = newText.value
+    addNewItem(newObj)
+    newText.value = ''
+  } else {
+    return
+  }
 });
 
-async function newMessage() {
-  let res = await fetch("https://606efbdf0c054f001765814c.mockapi.io/api/v1/messages");
-  let body = res.json();
-  return console.log('1');
+function addNewItem(newObj) {
+  if (lengthItems < 28) {
+    fetch(URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json;charset=utf-8' },
+      body: JSON.stringify(newObj)
+    })
+    cards.innerHTML = ''
+    loadItemsApi()
+  } else {
+    alert('you have maximum characters')
+  }
 }
+
+function deletItem(deletId) {
+  fetch(`${URL}/${deletId}`, {
+    method: 'DELETE',
+  });
+  cards.innerHTML = ''
+  loadItemsApi()
+};
+
+function setDomElements(body) {
+  body.forEach(el => {
+    const li = document.createElement('li')
+    const h3 = document.createElement('h3')
+    const span = document.createElement('span')
+    li.className = 'message'
+    li.value = el.id;
+    span.className = 'close'
+    cards.appendChild(li)
+    li.appendChild(h3)
+    li.appendChild(span)
+    h3.textContent = el.title;
+    span.textContent = 'X';
+
+    span.addEventListener('click', () => {
+      const element = cards.removeChild(li)
+      console.log(element.value);
+      deletItem(element.value)
+    });
+  })
+}
+
+function loadItemsApi() {
+  fetch(URL)
+    .then(response => response.json())
+    .then(body => {
+      lengthItems = body.length
+      setDomElements(body)
+    })
+    .catch(error => console.log(error))
+}
+
+loadItemsApi()
+
+
 
